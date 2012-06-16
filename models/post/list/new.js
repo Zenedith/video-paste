967d259a027aac76ceb4bd9b565debc45face287
Zenedith <zenedith@wp.pl>
2012-06-16 19:51:55 +0200

    - added cron task for clean older items in new list

--- models/post/list/new.js
+++ models/post/list/new.js
@@ -12,6 +12,51 @@ var Post_List_New = function ()
 
   Post_List.call(this);  //call parent constructor
 
+  this.cleanOlderPosts = function (callback) {
+    log.debug('Post_List_New.cleanOlderPosts()');
+
+    const MIN_NEW_LIST_SIZE = 100;
+
+    //check how many items on list
+    Database.countValuesInSet('new:posts', function (errCount, count) {
+
+      if (errCount) {
+        return callback(errCount, null);
+      }
+
+      //if not too many items, return
+      if (count < MIN_NEW_LIST_SIZE) {
+        return callback(null, 0);
+      }
+
+      //get new post ids
+      Database.getSortedValuesFromSet('new:posts', MIN_NEW_LIST_SIZE, -1, 'DESC', function (err, resList) {
+
+        if (err) {
+          return callback(err, null);
+        }
+
+        var
+          removeCount = resList.length;
+
+        //async
+        for (var i = 0; i < removeCount; ++i ) {
+
+          //TODO optimize: remove all ids at once
+          Database.removeValueFromSet('new:posts', resList[i], function(err, res) {
+            if (err) {
+              log.crit(err);
+            }
+          });
+        }
+
+        //dont wait to remove items
+        return callback(null, removeCount);
+      });
+    });
+
+  };
+
   this.get = function (limit, page, callback) {
     log.debug('Post_List_New.get(' + limit + ', ' + page + ')');
 
